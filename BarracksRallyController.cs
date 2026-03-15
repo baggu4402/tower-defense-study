a70bc32b1fddbf6b08de3dc07fce1f7dda16314c
baggu4402 <130174526+baggu4402@users.noreply.github.com>
2026-03-15 12:31:50 +0900

    Update BarracksRallyController.cs

--- BarracksRallyController.cs
+++ BarracksRallyController.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class BarracksRallyController : MonoBehaviour
 {
+    /*
+    Singleton 패턴
+
+    어디서든
+
+    BarracksRallyController.Instance
+
+    로 접근 가능
+    */
     public static BarracksRallyController Instance;
 
+    // 현재 선택된 병영
     private BarracksBuilding selectedBarracks;
 
     private void Awake()
     {
         Instance = this;
     }
+    
+    /*
+    Update()
 
+    병영이 선택된 상태에서
+
+    우클릭하면
+
+    새로운 rallyPoint 지정
+    */
     private void Update()
     {
         if (selectedBarracks == null)
@@ -18,8 +37,11 @@ public class BarracksRallyController : MonoBehaviour
 
         if (Input.GetMouseButtonDown(1))
         {
+            // 마우스 위치를 월드 좌표로 변환
             Vector2 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // 집결지 설정
             selectedBarracks.SetRallyPoint(worldPos);
+            
             selectedBarracks = null;
         }
     }
